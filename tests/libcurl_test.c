a934841081924593d1b28fb4231c9d51822d322e
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-11-19 23:16:29 +0300

    Simplify the libcurl test

--- tests/libcurl_test.c
+++ tests/libcurl_test.c
@@ -3,55 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  char *ptr;
-  size_t len;
-} string;
-
-void init_string(string *s) {
-  s->len = 0;
-  s->ptr = malloc(s->len + 1);
-  if (s->ptr == NULL) {
-    fprintf(stderr, "malloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  s->ptr[0] = '\0';
-}
-
-size_t write_callback(void *ptr, size_t size, size_t nmemb, string *s) {
-  size_t new_len = s->len + size * nmemb;
-  s->ptr = realloc(s->ptr, new_len + 1);
-  if (s->ptr == NULL) {
-    fprintf(stderr, "realloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  memcpy(s->ptr + s->len, ptr, size * nmemb);
-  s->ptr[new_len] = '\0';
-  s->len = new_len;
-
-  return size * nmemb;
-}
-
 int main(void) {
   CURL *curl;
   CURLcode res;
 
   curl = curl_easy_init();
   if (curl) {
-    string s;
-    init_string(&s);
+    char url[1024];
+    snprintf(url, 1024, "https://tile.openstreetmap.org/%d/%d/%d.png", 13, 5156, 3515);
 
-    curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+    printf("url = %s\n", url);
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.64.1");
 
     res = curl_easy_perform(curl);
 
     if (res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-
-    printf("%s\n", s.ptr);
-    free(s.ptr);
 
     curl_easy_cleanup(curl);
   }
