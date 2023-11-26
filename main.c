770b7f945a535e3d6cd9e9e8622fd1ee07acfd05
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-11-26 08:34:45 +0300

    refactor: Refactor code for better readability and maintainability

--- main.c
+++ main.c
@@ -16,168 +16,121 @@
 #define WIDTH 800
 #define HEIGHT 800
 
-SDL_Renderer *renderer = NULL;
-SDL_Texture *texture = NULL;
-
 static int long2tilex(double lon, int z) { return (int)(floor((lon + 180.0) / 360.0 * (1 << z))); }
 
 static int lat2tiley(double lat, int z) {
-  double latrad = lat * M_PI / 180.0;
-  return (int)(floor((1.0 - asinh(tan(latrad)) / M_PI) / 2.0 * (1 << z)));
+    double latrad = lat * M_PI / 180.0;
+    return (int)(floor((1.0 - asinh(tan(latrad)) / M_PI) / 2.0 * (1 << z)));
 }
 
 typedef struct {
-  char *ptr;
-  size_t len;
+    char *ptr;
+    size_t len;
 } string;
 
 static void init_string(string *s) {
-  s->len = 0;
-  s->ptr = malloc(s->len + 1);
-  if (s->ptr == NULL) {
-    fprintf(stderr, "malloc() failed\n");
-    exit(1);
-  }
-  s->ptr[0] = '\0';
+    s->len = 0;
+    s->ptr = malloc(s->len + 1);
+    if (s->ptr == NULL) {
+        fprintf(stderr, "malloc() failed\n");
+        exit(1);
+    }
+    s->ptr[0] = '\0';
 }
 
 static size_t write_callback(void *data, size_t size, size_t nmemb, void *user_data) {
-  size_t real_size = size * nmemb;
-  string *s = (string *)user_data;
+    size_t real_size = size * nmemb;
+    string *s = (string *)user_data;
 
-  size_t new_size = s->len + real_size;
-  s->ptr = realloc(s->ptr, new_size + 1);
+    size_t new_size = s->len + real_size;
+    s->ptr = realloc(s->ptr, new_size + 1);
 
-  if (s->ptr == NULL) {
-    fprintf(stderr, "realloc() failed\n");
-    exit(1);
-  }
-  memcpy(s->ptr + s->len, data, real_size);
-  s->ptr[new_size] = '\0';
-  s->len = new_size;
+    if (s->ptr == NULL) {
+        fprintf(stderr, "realloc() failed\n");
+        exit(1);
+    }
+    memcpy(s->ptr + s->len, data, real_size);
+    s->ptr[new_size] = '\0';
+    s->len = new_size;
 
-  return real_size;
+    return real_size;
 }
 
 static string curl_get_png_data(const char *url) {
-  CURL *curl = curl_easy_init();
+    CURL *curl = curl_easy_init();
 
-  if (!curl) {
-    fprintf(stderr, "[ERROR]: curl_easy_init() failed: %s\n", strerror(errno));
-    exit(1);
-  }
+    if (!curl) {
+        fprintf(stderr, "[ERROR]: curl_easy_init() failed: %s\n", strerror(errno));
+        exit(1);
+    }
 
-  string s;
-  init_string(&s);
+    string s;
+    init_string(&s);
 
-  curl_easy_setopt(curl, CURLOPT_URL, url);
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&s);
-  curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.64.1");
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&s);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.64.1");
 
-  CURLcode code = curl_easy_perform(curl);
+    CURLcode code = curl_easy_perform(curl);
 
-  if (code != CURLE_OK) {
-    fprintf(stderr, "[ERROR]: curl_easy_perform() failed: %s\n", curl_easy_strerror(code));
-    exit(1);
-  }
+    if (code != CURLE_OK) {
+        fprintf(stderr, "[ERROR]: curl_easy_perform() failed: %s\n", curl_easy_strerror(code));
+        exit(1);
+    }
 
-  curl_easy_cleanup(curl);
+    curl_easy_cleanup(curl);
 
-  return s;
-}
-
-int sdl_init(SDL_Renderer **r, SDL_Texture **t, string png_data) {
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "[ERROR]: could not initialize sdl: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-    fprintf(stderr, "[ERROR]: could not initialize sdl_image: %s\n", IMG_GetError());
-    return 1;
-  }
-
-  SDL_Window *window = SDL_CreateWindow("Map Viewer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-  if (window == NULL) {
-    fprintf(stderr, "[ERROR]: could not create sdl window: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if (renderer == NULL) {
-    fprintf(stderr, "[ERROR]: could not create sdl renderer: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  SDL_RWops *rw = SDL_RWFromConstMem(png_data.ptr, png_data.len);
-  if (rw == NULL) {
-    fprintf(stderr, "[ERROR]: could not create SDL_RWops: %s\n", SDL_GetError());
-    return 1;
-  }
-
-  SDL_Surface *surface = IMG_Load_RW(rw, 1); // 1 means SDL will free the RWops for us
-  if (surface == NULL) {
-    fprintf(stderr, "[ERROR]: could not load image: %s\n", IMG_GetError());
-    return 1;
-  }
-
-  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-  if (texture == NULL) {
-    fprintf(stderr, "[ERROR]: could not create texture from surface: %s", SDL_GetError());
-    return 1;
-  }
-
-  *r = renderer;
-  *t = texture;
-
-  return 0;
-}
-
-
-void sdl_cleanup() {
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
-  IMG_Quit();
-  SDL_Quit();
+    return s;
 }
 
 int main(void) {
-  const int tx = long2tilex(LNG, ZOOM); // convert longiture to tile x coordinate
-  const int ty = lat2tiley(LAT, ZOOM);  // convert latitude to tile y coordinate
+    const int tx = long2tilex(LNG, ZOOM); // convert longiture to tile x coordinate
+    const int ty = lat2tiley(LAT, ZOOM);  // convert latitude to tile y coordinate
 
-  char url[URL_BUF_SIZE];
-  snprintf(url, URL_BUF_SIZE, "https://tile.openstreetmap.org/%d/%d/%d.png", ZOOM, tx, ty);
+    char url[URL_BUF_SIZE];
+    snprintf(url, URL_BUF_SIZE, "https://tile.openstreetmap.org/%d/%d/%d.png", ZOOM, tx, ty);
 
-  string png_data = curl_get_png_data(url);
+    string png_data = curl_get_png_data(url);
 
-  if (sdl_init(&renderer, &texture, png_data) != 0) {
-    fprintf(stderr, "[ERROR]: could not create renderer and texture from png data\n");
-    return 1;
-  }
 
-  SDL_Event event;
-  bool quit = false;
-  while (!quit) {
-    SDL_RenderClear(renderer);
-		SDL_Rect rect = {};
-		SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
-    SDL_RenderCopy(renderer, texture, NULL, &rect);
+		// Initialisation
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
 
-    while (SDL_PollEvent(&event) != 0) {
-      if (event.type == SDL_QUIT) {
-        quit = true;
-      }
+    SDL_Window *window = SDL_CreateWindow("Map Viewer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_RWops *rw = SDL_RWFromConstMem(png_data.ptr, png_data.len);
+    SDL_Texture *texture = IMG_LoadTexture_RW(renderer, rw, 1);
+
+
+		// Event loop
+    SDL_Event event;
+    bool quit = false;
+    while (!quit) {
+        SDL_RenderClear(renderer);
+        SDL_Rect rect = {};
+        SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_RenderPresent(renderer);
     }
 
-    SDL_RenderPresent(renderer);
-  }
+		// Cleanup
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    IMG_Quit();
+    SDL_Quit();
 
-  sdl_cleanup();
+    free(png_data.ptr);
+    png_data.ptr = NULL;
+    png_data.len = 0;
 
-  free(png_data.ptr);
-  png_data.ptr = NULL;
-  png_data.len = 0;
-
-  return 0;
+    return 0;
 }
